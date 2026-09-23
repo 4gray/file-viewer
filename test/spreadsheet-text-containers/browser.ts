6502cc5e71513d11fcac07a0c74f5c9855379e4a
Yu, Wang <727842003@qq.com>
2026-09-23 23:45:38 +0800

    test(spreadsheet): resolve search controller in a frozen filtered workspace

--- test/spreadsheet-text-containers/browser.ts
+++ test/spreadsheet-text-containers/browser.ts
@@ -1,3 +1,3 @@
 import { renderFileViewerSpreadsheet } from '../../packages/renderers/spreadsheet/src/index.js';
-import { createFileViewerDomSearchController } from '@file-viewer/core';
+import { createFileViewerDomSearchController } from '../../packages/core/src/index.js';
 Object.assign(globalThis, { spreadsheetTextCheck: { renderFileViewerSpreadsheet, createFileViewerDomSearchController } });
